08a06b8ff4db97431bde63060e14c0c6574dcc64
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-05 11:28:43 -0700

    Create UI of basic calculator with buttons

--- javascript.js
+++ javascript.js
@@ -13,3 +13,18 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let num1, operator, num2;
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
