d870a319ed3ec224e5632b33fe29c21c15697e1f
Oi Ching Yip <67258773+yoic1239@users.noreply.github.com>
2024-02-05 14:09:37 -0700

    Make the calculator work
    Display number clicked and solution to corresponding operation

--- javascript.js
+++ javascript.js
@@ -14,9 +14,10 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-let num1, operator, num2;
-
 function operate(num1, operator, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
     switch(operator) {
         case "+":
             return add(num1, num2);
@@ -28,3 +29,34 @@ function operate(num1, operator, num2) {
             return divide(num1, num2);
     }
 }
+
+let num1, operator, num2;
+let displayVal = '';
+const displayBlk = document.querySelector('.display');
+const operators = document.querySelectorAll('.operators');
+const numbers = document.querySelectorAll('.numbers');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+
+numbers.forEach(number => number.addEventListener('click', function(){
+    displayVal += number.value;
+    displayBlk.textContent = displayVal;
+}));
+
+operators.forEach(btn => btn.addEventListener('click', function(){
+    num1 = (displayVal !== '') ? displayVal : displayBlk.textContent;
+    operator = btn.value;
+    displayVal = '';
+}));
+
+equal.addEventListener('click', function(){
+    num2 = displayVal;
+    displayVal = operate(num1, operator, num2);
+    displayBlk.textContent = displayVal;
+    displayVal = '';
+});
+
+clear.addEventListener('click', function() {
+    displayVal = '';
+    displayBlk.textContent = '0';
+});
